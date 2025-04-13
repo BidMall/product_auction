e68097eef2f7d0d54a77d09c04de4f92069150ce
taewoocode <taewoocode@gmail.com>
2025-04-13 22:57:17 +0900

    initial commit

---
+++ src/main/java/com/example/product_auction/ProductAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAuctionApplication.class, args);
+	}
+
+}
