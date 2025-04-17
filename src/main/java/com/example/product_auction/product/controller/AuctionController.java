ee1fc67dcf3f35a93d1ee5bc43b14bc4e82bd03b
taewoocode <taewoocode@gmail.com>
2025-04-17 20:00:59 +0900

    BM-9[feat]: 컨트롤러 조회 로직 구현

--- src/main/java/com/example/product_auction/product/controller/AuctionController.java
+++ src/main/java/com/example/product_auction/product/controller/AuctionController.java
@@ -3,40 +3,24 @@ package com.example.product_auction.product.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.example.product_auction.product.dto.AuctionRequest;
-import com.example.product_auction.product.dto.AuctionResponse;
+import com.example.product_auction.product.domain.Auction;
 
-@RestController
 public interface AuctionController {
 
-	@GetMapping("/{id}")
-	ResponseEntity<AuctionResponse> getAuction(@PathVariable Long id);
+	/**
+	 * @return 전체 경매 목록
+	 */
+	ResponseEntity<List<Auction.AuctionBaseResponse>> getAllAuctions();
 
-	@GetMapping
-	ResponseEntity<List<AuctionResponse>> getAllAuctions();
+	/**
+	 * @return 진행중 경매 목록
+	 */
+	ResponseEntity<List<Auction.OngoingAuctionResponse>> getOnGoingAuctions();
 
-	@PostMapping
-	ResponseEntity<AuctionResponse> createAuction(@RequestBody AuctionRequest request);
+	/**
+	 * @return 종료 경매 목록
+	 */
+	ResponseEntity<List<Auction.ClosedAuctionResponse>> getClosedAuctions();
 
-	@PutMapping("/{id}")
-	ResponseEntity<AuctionResponse> updateAuction(@PathVariable Long id, @RequestBody AuctionRequest request);
-
-	@PatchMapping("/{id}/delete")
-	ResponseEntity<Void> deleteAuctionLogic(@PathVariable Long id);
-
-	@DeleteMapping("/{id}")
-	ResponseEntity<Void> deleteAuctionPermanently(@PathVariable Long id);
-
-	@GetMapping("/status")
-	ResponseEntity<List<AuctionResponse>> getAuctionsByStatus(@RequestParam boolean isClosed);
 }
--- src/main/java/com/example/product_auction/product/controller/AuctionControllerImpl.java
+++ src/main/java/com/example/product_auction/product/controller/AuctionControllerImpl.java
@@ -4,19 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.product_auction.product.dto.AuctionRequest;
-import com.example.product_auction.product.dto.AuctionResponse;
+import com.example.product_auction.product.domain.Auction;
 import com.example.product_auction.product.service.AuctionService;
 
 import lombok.RequiredArgsConstructor;
@@ -31,132 +23,20 @@ public class AuctionControllerImpl implements AuctionController {
 	private final AuctionService auctionService;
 
 	/**
-	 * 단일 경매 조회
-	 * @param id
-	 * @return
-	 */
-	@Override
-	@GetMapping("/{id}")
-	public ResponseEntity<AuctionResponse> getAuction(
-		@PathVariable Long id) {
-		try {
-			AuctionResponse auctionById = auctionService.getAuctionById(id);
-			log.info("단일 경매 조회={}", auctionById);
-			return ResponseEntity.ok(auctionById);
-		} catch (Exception e) {
-			log.error("단일 경매 실패={}", e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
-	}
-
-	/**
-	 * 전체 경매 목록 조회
-	 * @return
+	 * 전체 경매 목록 조회 (진행 중, 종료된 경매 모두 포함)
+	 *
+	 * @return 전체 경매 목록
 	 */
 	@Override
 	@GetMapping
-	public ResponseEntity<List<AuctionResponse>> getAllAuctions() {
+	public ResponseEntity<List<Auction.AuctionBaseResponse>> getAllAuctions() {
 		try {
-			List<AuctionResponse> auctions = auctionService.getAllAuction();
-			log.info("경매 목록 조회={}", auctions);
+			List<Auction.AuctionBaseResponse> auctions = auctionService.findAllByAuctions();
+			log.info("경매 목록 조회 성공 총 ={}건", auctions.size());
 			return ResponseEntity.ok(auctions);
 		} catch (Exception e) {
-			log.error("경매 생성 실패={}", e);
+			log.error("경매 목록 조회 실패={}", e.getMessage(), e);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
-	}
-
-	/**
-	 * 경매생성
-	 * @param request
-	 * @return
-	 */
-	@Override
-	@PostMapping
-	public ResponseEntity<AuctionResponse> createAuction(
-		@RequestBody AuctionRequest request) {
-		try {
-			AuctionResponse auction = auctionService.createAuction(request);
-			log.info("경매 성공 성공={}", auction);
-			return ResponseEntity.status(HttpStatus.CREATED).body(auction);
-		} catch (Exception e) {
-			log.error("경매 생성 실패={}", e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
-	}
-
-	/**
-	 * 경매 수정
-	 * @param id
-	 * @param request
-	 * @return
-	 */
-	@Override
-	@PutMapping("/{id}")
-	public ResponseEntity<AuctionResponse> updateAuction(
-		@PathVariable Long id,
-		@RequestBody AuctionRequest request) {
-		try {
-			AuctionResponse auctionResponse = auctionService.updateAuction(id, request);
-			log.info("경매 수정 성공={}", auctionResponse);
-			return ResponseEntity.status(HttpStatus.CREATED).body(auctionResponse);
-		} catch (Exception e) {
-			log.error("경매 수정 실패={}", e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
-	}
-
-	/**
-	 * 경매 논리 삭제
-	 * @param id
-	 * @return
-	 */
-	@Override
-	@PatchMapping("/{id}/delete")
-	public ResponseEntity<Void> deleteAuctionLogic(@PathVariable Long id) {
-		try {
-			auctionService.deleteAuction(id);
-			log.info("경매 논리 삭제 성공: " + id);
-			return ResponseEntity.noContent().build();
-		} catch (Exception e) {
-			log.error("경매 논리 삭제 실패", e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-
-	/**
-	 * 경매 영구 삭제
-	 * @param id
-	 * @return
-	 */
-	@Override
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteAuctionPermanently(@PathVariable Long id) {
-		try {
-			auctionService.deleteAuctionPermanently(id);
-			log.info("경매 영구 삭제 성공: " + id);
-			return ResponseEntity.noContent().build();
-		} catch (Exception e) {
-			log.error("경매 영구 삭제 실패", e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-
-	/**
-	 * 경매 상태(isClosed)에 따른 조회
-	 * @param isClosed
-	 * @return
-	 */
-	@Override
-	@GetMapping("/status")
-	public ResponseEntity<List<AuctionResponse>> getAuctionsByStatus(@RequestParam boolean isClosed) {
-		try {
-			List<AuctionResponse> auctions = auctionService.getAuctionsByClosedStatus(isClosed);
-			log.info("경매 상태 조회 성공, isClosed: {}", isClosed);
-			return ResponseEntity.ok(auctions);
-		} catch (Exception e) {
-			log.error("경매 상태 조회 실패, isClosed: {}", isClosed, e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 	}
 }
