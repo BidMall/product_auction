5fea072576d60cb17f829c224b1f6bce335c1846
taewoocode <taewoocode@gmail.com>
2025-04-18 03:58:11 +0900

    BM-9[feat]: 입찰정보 레포지토리 추가

--- src/main/java/com/example/product_auction/product/repository/BidRepository.java
+++ src/main/java/com/example/product_auction/product/repository/BidRepository.java
@@ -3,13 +3,19 @@ package com.example.product_auction.product.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+가import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.product_auction.product.domain.Auction;
 import com.example.product_auction.product.domain.Bid;
 
 @Repository
 public interface BidRepository extends JpaRepository<Bid, Long> {
+	List<Bid> findByAuctionId(Long request);
 
-	public List<Bid> findByAuctionId(Long auctionId);
+	@Query("SELECT b FROM Bid b WHERE b.auction.id = :auctionId AND b.auction.status = :status")
+	List<Bid> findByAuctionIdAndAuctionStatus(@Param("auctionId") Long auctionId,
+		@Param("status") Auction.AuctionStatus status);
 
 }
